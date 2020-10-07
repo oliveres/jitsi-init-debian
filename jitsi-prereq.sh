10db1535a16138771f2e5892e4bf1f0c48db112a
lemocloud <72502114+lemocloud@users.noreply.github.com>
2020-10-07 15:32:21 +0200

    Update jitsi-prereq.sh

--- jitsi-prereq.sh
+++ jitsi-prereq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-userdel jitsiuser
+
 apt install apt-transport-https mc curl gpg lsb-release -y
 # wget https://www.lemo.cloud/download/authorized_keys
 # mkdir -p /root/.ssh
@@ -7,4 +7,6 @@ apt install apt-transport-https mc curl gpg lsb-release -y
 # /etc/init.d/ssh restart
 curl https://download.jitsi.org/jitsi-key.gpg.key | sh -c 'gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg' 
 echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
+echo "Nyni si zmente heslo uzivatele root"
+passwd
 # end of script
