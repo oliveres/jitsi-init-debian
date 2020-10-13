5449898b28c07c76ab51a345065fa056e6a81340
lemocloud <72502114+lemocloud@users.noreply.github.com>
2020-10-13 14:28:09 +0200

    Update jitsi-afterlogin.sh

--- jitsi-afterlogin.sh
+++ jitsi-afterlogin.sh
@@ -2,12 +2,12 @@ ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 echo "***************************************"
 echo "* Vitejte v konzoli serveru pro Jitsi *"
 echo "***************************************"
-echo "\\n"
+echo "\n"
 echo "
 Predpokladem pro uspesnou instalaci je, ze jste u vaseho spravce domeny vytvorili domenu treti urovne
 (napriklad videokonference.vasefirma.cz) formou A zaznamu smerujici na verejnou IP adresu tohoto serveru
-ktera je $ip a kterou rovnez vidite v administraci LEMO Cloud, a take jste pockali az 24 hodin na rozsireni
-DNS zaznamu mezi servery v internetu.
+ktera je $ip a kterou rovnez vidite v administraci LEMO Cloud, a take jste pockali az 24 hodin
+na rozsireni DNS zaznamu mezi servery v internetu.
 
 Pokud jste pripraveni, instalaci zahajite napsanim prikazu ./jitsi_cloudinit.sh
 "
