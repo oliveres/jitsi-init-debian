5cdccde856c39fa9f63a9b8ed09cf6e919ce6d5e
lemocloud <72502114+lemocloud@users.noreply.github.com>
2020-10-13 16:09:07 +0200

    Update jitsi-afterlogin.sh

--- jitsi-afterlogin.sh
+++ jitsi-afterlogin.sh
@@ -7,6 +7,6 @@ Predpokladem pro uspesnou instalaci je, ze jste u vaseho spravce domeny vytvoril
 (napriklad videokonference.vasefirma.cz) formou A zaznamu smerujici na verejnou IP adresu tohoto serveru
 ktera je $ip a kterou rovnez vidite v administraci LEMO Cloud, a take jste pockali az 24 hodin
 na rozsireni DNS zaznamu mezi servery v internetu."
-echo -e "\e[1;31m Nejdrive si zmente heslo uzivatele root zadanim prikazu passwd \e[0m"
+echo -e "\e[1;31mNejdrive si zmente heslo uzivatele root zadanim prikazu passwd\e[0m"
 echo ""
-echo"Pokud mate heslo zmenene a domenu funkcni, instalaci zahajite zadanim prikazu ./jitsi-install.sh"
+echo "Pokud mate heslo zmenene a domenu funkcni, instalaci zahajite zadanim prikazu ./jitsi-install.sh"
