1eb791fd6e1fec9cd3b5d88173c9519235ed2c97
lemocloud <72502114+lemocloud@users.noreply.github.com>
2020-10-13 16:15:59 +0200

    Update jitsi-install.sh

--- jitsi-install.sh
+++ jitsi-install.sh
@@ -3,7 +3,7 @@ curl https://download.jitsi.org/jitsi-key.gpg.key | sh -c 'gpg --dearmor > /usr/
 echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
 apt update
 apt upgrade -y
-
+apt install apt-transport-https mc curl gpg lsb-release -y
 ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 echo "**********************************************************"
 echo "* Vita vas instalace Jitsi serveru beziciho v LEMO Cloud *"
@@ -24,7 +24,6 @@ then
 fi 
 echo "DNS zaznam je v poradku, nyni bude zahajena samotna instalace. V jejim prubehu jeste jednou zadate $hostname"
 sleep 5
-# apt update
 # apt install jitsi-meet -y
 # echo -e "letsencrypt@lemo.cloud" | /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 echo "Instalace je dokoncena. Server bude restartovan."
